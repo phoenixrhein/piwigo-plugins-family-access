e83096706f402d1bbc89ba4ffb330fa6d54f514f
phoenixrhein <76103235+phoenixrhein@users.noreply.github.com>
2020-12-24 07:35:58 +0100

    Cosmetic adjustments

--- include/FamilyAccessTemplateHelper.php
+++ include/FamilyAccessTemplateHelper.php
@@ -26,4 +26,32 @@ class FamilyAccessTemplateHelper
         global $template;
         return $template;
     }
+
+    /**
+     * Clear variables
+     *
+     * @param array $variables
+     * @return void
+     */
+    public function clearVariables(array $variables)
+    {
+        foreach ($variables as $variable) {
+            $this->getTemplate()->assign($variable, '');
+        }
+    }
+
+    /**
+     * Remove variables
+     *
+     * @param array $variables
+     * @return void
+     */
+    public function removeVariables(array $variables)
+    {
+        foreach ($variables as $variable) {
+            if (array_key_exists($variable, $this->getTemplate()->smarty->tpl_vars) === true) {
+                unset($this->getTemplate()->smarty->tpl_vars[$variable]);
+            }
+        }
+    }
 }
--- view/FamilyAccessQuestionView.php
+++ view/FamilyAccessQuestionView.php
@@ -1,95 +1,163 @@
 <?php
+// ############################################################################
+// *
+// * Copyright (C) xt by hobutech
+// *
+// ############################################################################
+
 // Chech whether we are indeed included by Piwigo.
 if (defined('PHPWG_ROOT_PATH') === false) {
     die('Hacking attempt!');
 }
 
+/**
+ * class FamilyAccessQuestionView
+ */
 class FamilyAccessQuestionView
 {
 
     /**
-     * 
+     *
      * @var FamilyAccessTemplateHelper
      */
     private $templateHelper;
-    
+
     /**
-     * 
+     *
      * @var string
      */
     private $title;
-    
+
     /**
-     * 
+     *
      * @var string
      */
     private $imageUrl;
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         $this->templateHelper = new FamilyAccessTemplateHelper();
     }
 
+    /**
+     * View
+     *
+     * @param string $pageTitle
+     * @param string $imageUrl
+     * @return void
+     */
     public function view($pageTitle, $imageUrl = null)
     {
         $this->title = $pageTitle;
         $this->imageUrl = $imageUrl;
-        $this->disableCategoryListView(); 
-        
+        $this->disableCategoryListView();
+
         $filePath = FAMILY_ACCESS_TEMPLATE_PATH . 'access-question.tpl';
         $this->templateHelper->getTemplate()->set_filename('accessquestion', $filePath);
         $this->templateHelper->getTemplate()->assign('url', $this->prepareUrl());
         $this->templateHelper->getTemplate()->assign_var_from_handle('CATEGORIES', 'accessquestion');
-        
     }
-    
-    private function prepareUrl() 
+
+    /**
+     * Get prepared forward url
+     *
+     * @return string
+     */
+    private function prepareUrl()
     {
         $url = 'http';
-        if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!='off') {
+        if (! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
             $url = 'https';
         }
         $url .= '://' . $_SERVER['HTTP_HOST'];
-        if (array_key_exists('pwg_familyaccess_forward_url', $_SESSION) === true && $_SESSION['pwg_familyaccess_forward_url'] != '' ) {
+        if (array_key_exists('pwg_familyaccess_forward_url', $_SESSION) === true && $_SESSION['pwg_familyaccess_forward_url'] != '') {
             $url = $_SESSION['pwg_familyaccess_forward_url'];
         }
-        
+
         return $url;
     }
 
+    /**
+     * Disable category list view
+     *
+     * @return void
+     */
     private function disableCategoryListView()
     {
-        $this->templateHelper->getTemplate()->assign('cats_navbar', '');
-        $this->templateHelper->getTemplate()->assign('thumb_navbar', '');
-        $this->templateHelper->getTemplate()->assign('image_derivatives', '');
-        $this->templateHelper->getTemplate()->assign('image_orders', '');
-        $this->templateHelper->getTemplate()->assign('PLUGIN_INDEX_BUTTONS', '');
-        $this->templateHelper->getTemplate()->assign('PLUGIN_INDEX_ACTIONS', '');
+        $clearVariables = [
+            'cats_navbar',
+            'thumb_navbar',
+            'image_derivatives',
+            'image_orders',
+            'PLUGIN_INDEX_BUTTONS',
+            'PLUGIN_INDEX_ACTIONS'
+        ];
 
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_MODE_CREATED']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['favorite']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_EDIT']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_CADDIE']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_SEARCH_RULES']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_SLIDESHOW']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_MODE_FLAT']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_MODE_NORMAL']);
-        unset($this->templateHelper->getTemplate()->smarty->tpl_vars['U_MODE_POSTED']);
+        $this->templateHelper->clearVariables($clearVariables);
+
+        $removeVariables = [
+            'U_MODE_CREATED',
+            'favorite',
+            'U_EDIT',
+            'U_CADDIE',
+            'U_SEARCH_RULES',
+            'U_SLIDESHOW',
+            'U_MODE_FLAT',
+            'U_MODE_NORMAL',
+            'U_MODE_POSTED'
+        ];
+
+        $this->templateHelper->removeVariables($removeVariables);
     }
-    
+
+    /**
+     * Disable menubar
+     *
+     * @return void
+     */
     public function disableMenubar()
     {
         $this->templateHelper->getTemplate()->smarty->tpl_vars['MENUBAR'] = new Smarty_Variable('', false);
-        
-        $this->templateHelper->getTemplate()->assign('GALLERY_TITLE', 'Fotos der Familie HKS2');
+        $this->setTitle();
+        $this->setMetaTagImage();
+    }
+
+    /**
+     * Set page title
+     *
+     * @return void
+     */
+    private function setTitle()
+    {
+        $this->templateHelper->getTemplate()->assign('GALLERY_TITLE', 'Fotos der Familie HKS');
         $this->templateHelper->getTemplate()->assign('PAGE_TITLE', $this->title);
-        if ($this->imageUrl != null) {
-            $url = 'https://'.$_SERVER['SERVER_NAME'].$this->imageUrl;
-            //nicht einfach head elements ueberschreiben?
-            $this->templateHelper->getTemplate()->assign('head_elements', array('<meta property="og:image" content="' . $url . '" />'));
+    }
+
+    /**
+     * Set meta tag image
+     *
+     * @return void
+     */
+    private function setMetaTagImage()
+    {
+        if ($this->imageUrl == null) {
+            return;
         }
-        
-        
-        
+
+        $url = 'https://' . $_SERVER['SERVER_NAME'] . $this->imageUrl;
+        $headElements = [
+            '<meta property="og:image" content="' . $url . '" />'
+        ];
+        if (array_key_exists('head_elements', $this->templateHelper->getTemplate()->smarty->tpl_vars) === true) {
+            /** @var $headElements Smarty_Variable */
+            $headElementObj = $this->templateHelper->getTemplate()->smarty->tpl_vars['head_elements'];
+            $headElements = array_merge($headElementObj->value, [
+                '<meta property="og:image" content="' . $url . '" />'
+            ]);
+        }
+        $this->templateHelper->getTemplate()->assign('head_elements', $headElements);
     }
 }
